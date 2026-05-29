e72d49e67422691571b102f77cdbe670a59d3834
VailetiDineshKumar <vdkdinesh2615@gmail.com>
2026-05-29 16:41:24 +0530

    Add various Java programs for basic operations and functionalities
    - Created a program to demonstrate variable types and their values (vari.java).
    - Implemented a voting system that allows users to cast votes for candidates based on age eligibility (votes.java).
    - Added a program to calculate time difference between two given times (timedif.java).
    - Developed a program to determine letter grades based on input scores (terGrade.java).
    - Implemented a multiplication table generator (tables.java).
    - Created a switch-case based program to determine train compartments based on input (switchtrain.java).
    - Added a program to perform bitwise operations using switch-case (switchop.java).
    - Developed a simple calculator using switch-case for basic arithmetic operations (switchcal.java).
    - Implemented a program to swap adjacent elements in an array (swapadj.java).
    - Created a program to check for spy numbers (spy.java).
    - Added a program to play rock-paper-scissors (rps.java).
    - Implemented a quadrant determination program based on user input (quadrant.java).
    - Developed a program to reverse a string (reverse.java).
    - Created a program to print alternating uppercase letters from a string (st.java).
    - Added a program to check for adjacent swaps in an array (swapadj.java)

---
+++ Etnotech/threePointers.java
@@ -0,0 +1,36 @@
+class p2{
+ public static void main(String[] args){
+    int[] arr1={1,2,3,4,5,6};
+    int[] arr2={2,4,6,8,10};
+    int[] arr3={2,3,6,9,12};
+    // for(int i : arr1){
+    //     for(int j :arr2){
+    //         if(i==j){
+    //             for(int k: arr3){
+    //                 if(i==k){
+    //                     System.out.print(i+" ");
+    //                 }
+    //             }
+    //         }
+    //     }
+    // }
+    int i=0,j=0,k=0;
+    while(i<arr1.length && j<arr2.length && k<arr3.length){
+        if(arr1[i]==arr2[j] && arr2[j]==arr3[k]){
+            System.out.print(arr1[i]+" ");
+            i++;
+            j++;
+            k++;
+        }
+        else if(arr1[i]<arr2[j]){
+            i++;
+        }
+        else if(arr2[j]<arr3[k]){
+            j++;
+        }
+        else{
+            k++;
+        }
+    }
+ }   
+}
