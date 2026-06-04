18d60e7e2f11f5627508429b6a7907d7b0aa6b53
VailetiDineshKumar <vdkdinesh2615@gmail.com>
2026-06-04 12:31:38 +0530

    Add Fibonacci check and new pattern printing program

---
+++ Etnotech/p5.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++){
+            if(i!=n){
+            for(int j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            for(int j=n-1;j>=i;j--){
+                System.out.print("  ");
+            }
+            for(int j=n-1;j>i;j--){
+                System.out.print("  ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        else{
+            for(int j=1;j<=n*2-1;j++){
+                System.out.print("* ");
+            }
+        }
+        }
+    }
+}
--- Rampex/fibonot.java
+++ Rampex/fibonot.java
@@ -1,23 +1,24 @@
 import java.util.*;
 class fibonacci {
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        int n = s.nextInt();
-        int a = 0, b = 1, f=0, c;
-        while(a<=n) { 
-            c = a + b;
-            if(n==a){
-                f=1;
-                break;   
+        try (Scanner s = new Scanner(System.in)) {
+            int n = s.nextInt();
+            int a = 0, b = 1, f=0, c;
+            while(a<=n) { 
+                c = a + b;
+                if(n==a){
+                    f=1;
+                    break;   
+                }
+                a = b;
+                b = c;
             }
-            a = b;
-            b = c;
-        }
-        if(f==1){
-            System.out.println("Fibonacci");
-        }
-        else{
-            System.out.println("Not a Fibonacci");
+            if(f==1){
+                System.out.println("Fibonacci");
+            }
+            else{
+                System.out.println("Not a Fibonacci");
+            }
         }
     }
 }
