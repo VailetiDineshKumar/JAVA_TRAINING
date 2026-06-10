0382534e67b5768349ada2480f4236ccb0311528
VailetiDineshKumar <vdkdinesh2615@gmail.com>
2026-06-10 12:10:04 +0530

    Add linkedlist class with LinkedList operations and user input handling

--- Etnotech/Synus/LL/linkedlist.java
+++ Etnotech/Synus/LL/linkedlist.java
@@ -1,5 +1,22 @@
 package Synus.LL;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.*;
 public class linkedlist {
-    
+    public static void main(String[] args){
+        LinkedList<String> list = new LinkedList<>();
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        for(int i=0; i<n; i++){
+            list.add(s.next());
+        }
+        System.out.println("LinkedList: " + list);
+        list.remove("bike");
+        System.out.println("After removing 'bike': " + list);
+        System.out.println("Size of LinkedList: " + list.size());
+        list.addLast("last_element");
+        list.addFirst("First");
+        list.add(1,"second");
+        System.out.println(list);
+    }
 }
