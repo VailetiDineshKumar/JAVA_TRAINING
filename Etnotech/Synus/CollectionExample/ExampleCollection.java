91edf73c57a107772bdfb32c26c37d06a9aa0fd8
VailetiDineshKumar <vdkdinesh2615@gmail.com>
2026-06-11 14:15:41 +0530

    Add ExampleCollection class demonstrating HashSet, TreeSet, and LinkedHashSet usage

---
+++ Etnotech/Synus/CollectionExample/ExampleCollection.java
@@ -0,0 +1,41 @@
+package Synus.CollectionExample;
+import java.util.*;
+public class ExampleCollection {
+    public static void main(String[] args) {
+        // Example of using an ArrayList
+       System.out.println("Set programs - HashSet , TreeSet , LinkedHashSet");
+       
+       Set<Integer> RollNo = new HashSet<>();
+       RollNo.add(1);
+       RollNo.add(2);   
+       RollNo.add(3);
+       System.out.println("This is for Set ");
+       System.out.println("==============HashSet==============");
+       HashSet<Integer> hashset = new HashSet<>();
+       hashset.add(101);
+       hashset.add(102);
+       hashset.add(103);
+        System.out.println("HashSet is for maintaining unique elements and does not maintain any order.");
+        System.out.println(hashset.contains(103));
+        System.out.println("The hashset numbers are: " + hashset);
+        System.out.println("The size of the hashset is: " + hashset.size());
+        System.out.println("==============TreeSet==============");
+        TreeSet<Integer> treeset = new TreeSet<>();
+        treeset.add(201);
+        treeset.add(202);
+        treeset.add(203);
+        System.out.println("TreeSet is for maintaining unique elements and sorts them in natural order.");
+        System.out.println(treeset.contains(202));
+        System.out.println("The treeset numbers are: " + treeset);
+        System.out.println("The size of the treeset is: " + treeset.size());
+        LinkedHashSet<Integer> linkedhashset = new LinkedHashSet<>();
+        linkedhashset.add(301); 
+        linkedhashset.add(302);
+        linkedhashset.add(303);
+        System.out.println("LinkedHashSet is for maintaining unique elements and preserves the insertion order.");
+        System.out.println(linkedhashset.contains(302));
+        System.out.println("The linkedhashset numbers are: " + linkedhashset);
+        System.out.println("The size of the linkedhashset is: " + linkedhashset.size());
+         
+    }
+}
