b637d6a0ff74afa6e37e7225047935b33a7417df
VailetiDineshKumar <vdkdinesh2615@gmail.com>
2026-06-24 16:08:29 +0530

    Remove volatile modifier from number variable in VolatileReordering class

--- Etnotech/Synus/Debugging/VolatileReordering.java
+++ Etnotech/Synus/Debugging/VolatileReordering.java
@@ -2,7 +2,7 @@ package Synus.Debugging;
 import java.util.*;
 public class VolatileReordering {
     private static  boolean ready = false;
-    private static volatile int number = 0;
+    private static int number = 0;
 
     public static void main(String[] args) {
         new Thread(() -> {
